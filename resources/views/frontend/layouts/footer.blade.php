193d3e2768a1618b595ee330069d0d186fe87eff
Arsal <arsalkamoka786@gmail.com>
2025-05-19 16:46:29 +0500

    changes

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -4,19 +4,21 @@
         <div class="footer-top">
             <div class="widget-section">
                 <div class="row">
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
                         <div class="logo-widget footer-widget">
                             <figure class="footer-logo"><a href="{{url('/')}}"><img src="{{asset('images/settings/'.adminSettings('admin_app_logo_white'))}}" width="100px" height="100px" alt=""></a></figure>
                             <div class="text">Over 15 years of experience we’ll ensure you always get the best guidance.</div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
                         <div class="links-widget company-widget footer-widget">
                             <h4 class="widget-title">Quick navigation</h4>
                             <div class="widget-content">
                                 <ul>
                                     <li><a href="{{ url('/team') }}">Home</a></li>
                                     <li><a href="{{ url('/about') }}">About</a></li>
+                                    <li><a href="{{ url('/team') }}">Team</a></li>
+                                    <li><a href="{{ url('/blog') }}">Blogs</a></li>
                                     <li><a href="{{ url('contact') }}">Contact</a></li>
                                     <li><a href="{{  url('/term-conditions') }}">Term & Conditions</a></li>
                                     <li><a href="{{ url('privacy-policy') }}">Privacy Policy</a></li>                                    
@@ -25,7 +27,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
                         <div class="links-widget footer-widget">
                             <h4 class="widget-title">Categorize </h4>
                             <div class="widget-content">
@@ -42,8 +44,8 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                        <div class="links-widget footer-widget">
+                    {{-- <div class="col-lg-3 col-md-6 col-sm-12 footer-column"> --}}
+                        {{-- <div class="links-widget footer-widget">
                             <h4 class="widget-title">Industries</h4>
                             <div class="widget-content">
                                 <ul>
@@ -55,7 +57,7 @@
                                     <li><a href="#">Building Management</a></li>
                                 </ul>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
@@ -63,11 +65,12 @@
         <div class="footer-bottom clearfix">
             <div class="copyright pull-left">{{adminSettings('admin_footer_text')}}</div>
             <ul class="footer-social pull-right">
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
                 <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                <li><a href="#"><i class="fab fa-google"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>            
+                
             </ul>
         </div>
     </div>
