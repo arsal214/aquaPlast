23d0f5b617d83c77e83b82548267be9a57f98b46
Arsal <arsalkamoka786@gmail.com>
2025-05-23 21:03:17 +0500

    change

--- resources/views/frontend/blog-single.blade.php
+++ resources/views/frontend/blog-single.blade.php
@@ -1,47 +1,49 @@
 @extends('frontend.layouts.app')
 
 @section('template_title')
-    Aqua Plast | Blog Detail . {{$blog->title}}
+    Aqua Plast | Blog Detail . {{ $blog->title }}
 @endsection
 
 @section('content')
-@if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 
-@if(session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
-@endif
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
 
+    <div class="container">
+        <div class="content-box">
+            <h1>{{ $blog->title }}</h1>
+        </div>
+    </div>
     <!-- blog-single -->
     <section class="blog-single sidebar-page-container">
         <div class="col mx-auto news-block">
-            <figure class="image-box"><a href="{{route('blogShow',$blog->id)}}">
-                <div style="text-align: center;">
-                <img src="{{$blog->image}}" alt="" style="max-width:75%"></a>
-                </div>
-            </figure>
+            <figure class="image-box"><a href="{{ route('blogShow', $blog->id) }}">
+                    <div style="text-align: center;">
+                        <img src="{{ $blog->image }}" alt="" style="max-width:75%">
+                </a>
+        </div>
+        </figure>
         </div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 content-side">
                     <div class="blog-single-content">
 
-                        <div class="container">
-                            <div class="content-box">
-                                <h1>{{ $blog->title }}</h1>
-                            </div>
-                        </div>
+
                         {!! $blog->body !!}
 
                         <div class="comments-form-area">
                             <h2 class="group-title">Leave Reply</h2>
                             <div class="text">Your email address will not be published. Required fields are marked *</div>
-                            <form action="{{ route('blogComment',$blog->id) }}" method="post" class="comment-form">
+                            <form action="{{ route('blogComment', $blog->id) }}" method="post" class="comment-form">
                                 @csrf
                                 <div class="row">
                                     <div class="col-lg-12 col-md-12 col-sm-12 form-group">
@@ -65,5 +67,4 @@
         </div>
     </section>
     <!-- blog-single end -->
-
 @endsection
