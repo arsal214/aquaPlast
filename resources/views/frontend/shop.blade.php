168dc2a606e06f8deb3629e7ca42bddcd0c68d96
Arsal <arsalkamoka786@gmail.com>
2025-05-23 20:51:15 +0500

    chn

--- resources/views/frontend/shop.blade.php
+++ resources/views/frontend/shop.blade.php
@@ -10,31 +10,13 @@
     <!-- shop-banner -->
     <section class="shop-banner">
         <div class="main-slider-carousel owl-carousel owl-theme">
-            <div class="slide" style="background-image:url(frontend/images/main-slider/slider-10.jpg)">
+            <div class="slide" style="background-image:url({{ asset('images/settings/'.adminSettings('product_page_banner_image')) }});">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-7 col-md-12 col-sm-12 content-column">
                             <div class="content-box">
                                 <h1>water jet pumps<br />bare pumps</h1>
                                 <div class="text">This Center Lathe Machine supplied by us is highly used for the<br />purpose of producing concentric work. This product is a highly<br />demanded product in the</div>
-                            </div>
-                        </div>
-                        <div class="col-lg-5 col-md-12 col-sm-12 image-column">
-                            <div class="image-box">
-                                <figure class="image clearfix"><img src="frontend/images/resource/machine-2.png" alt=""></figure>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slide" style="background-image:url(frontend/images/main-slider/slider-10.jpg)">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-7 col-md-12 col-sm-12 content-column">
-                            <div class="content-box">
-                                <h1>water jet pumps<br />bare pumps</h1>
-                                <div class="text">This Center Lathe Machine supplied by us is highly used for the<br />purpose of producing concentric work. This product is a highly<br />demanded product in the</div>
-                                <div class="btn-box"><a href="product-show.blade.php">Add To Cart</a></div>
                             </div>
                         </div>
                         <div class="col-lg-5 col-md-12 col-sm-12 image-column">
